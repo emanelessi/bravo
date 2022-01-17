782f599d29d247e8f6632653fe5e05f07381802d
emanelessi <hanieman86@gmail.com>
2022-01-17 23:05:54 +0200

    first commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,6 +26,7 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+
         if (! $this->app->routesAreCached()) {
             Passport::routes();
         }
